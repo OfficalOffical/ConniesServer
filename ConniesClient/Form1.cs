5579e632c142be68dab7f00360deb4f1ded145cb
Sefa <Sefa@DESKTOP-O4J4T5L>
2021-10-30 11:35:17 +0300

    Proje dosyası ekle.

---
+++ ConniesClient/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace ConniesClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            clientMessage();
+        }
+
+        void clientMessage()
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 1071);
+                NetworkStream stream = client.GetStream();
+
+                StreamWriter sw = new StreamWriter(stream);
+                StreamReader sr = new StreamReader(stream);
+
+
+                sw.WriteLine("Sended from Client");
+                sw.Flush();
+
+
+
+                
+
+                stream.Close();
+                client.Close();
+
+               
+
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine("Failed",e);
+            }
+
+        }
+
+    }
+}
---
+++ ConniesServer/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace ConniesServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listener(1071);
+
+        }
+        // Server side 
+        void listener(int port)
+        {
+            try
+            {
+
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                TcpListener server = new TcpListener(localAddr, port);
+                server.Start();
+
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+                    TcpClient client = server.AcceptTcpClient();
+                    // change here for rdt 1.0 2.0 etc
+
+                    Debug.WriteLine("Connected!");              
+              
+                    NetworkStream stream = client.GetStream();
+                    StreamReader sr = new StreamReader(stream);
+                    StreamWriter sw = new StreamWriter(stream);
+
+                    string valueReaded = sr.ReadToEnd();
+                    Debug.WriteLine(valueReaded);
+
+                    
+
+                    stream.Close();
+                    client.Close();
+
+                }
+            
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+     
+
+    }
+}
